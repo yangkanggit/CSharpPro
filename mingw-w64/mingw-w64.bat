5ac8479ca3a95bb881fd4eb514f316d0400503d3
yangkangems@outlook.com <yangkangems@outlook.com>
2020-01-22 23:57:44 +0800

    change ming

--- mingw-w64/mingw-w64.bat
+++ mingw-w64/mingw-w64.bat
@@ -1,6 +1,6 @@
 echo off
-set PATH=C:\mingw-w64\mingw32\bin;%PATH%
+set PATH=C:\mingw-w64\mingw64\bin;%PATH%
 rem echo %PATH%
-rem cd "C:\mingw-w64\mingw32\bin"
+rem cd "C:\mingw-w64\mingw64\bin"
 cd "C:\"
 "C:\Windows\system32\cmd.exe"
